ae95c2220c80787f79c5d5b804ed54e6bf979dbe
Angelica <mmaderacovo@gmail.com>
2023-05-16 17:02:38 -0500

    Variables en TypeScript

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+let myTypeString: string='Hello World';
+let myTypeNumber: number=23;
+let MyTypeBolean: boolean= true;
+
+// Array
+let arrNumber: number[]=[1,2,3];
+//let arrNumber2: Array<number>=[1,2,3];
+let arrString: string[]=["a","b","c"];
+
+let arrAny: any[]=["a",2,"b"];
+
+//Tuple
+let tuplePlayers: [string, number, boolean]=['Angelica',6,true];
+
+//Tuple Array
+let players: [number,string][];
+
+players= [
+    [1,'Angelica'],
+    [2,'Juan'],
+    [3,'Pablo']
+];
+
+let myVariable;
+
+let myVariable1:string;
+
+let myVariable2= 'Hola mundo';
+
+//Unions
+let myVariable3: string | number | null;
+myVariable3='angelica';
+myVariable3= 34;
+
+
+
+
